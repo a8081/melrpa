abaead9d161cfe78671b6d1c89056475d62030f9
a8081 <amartinezr98@protonmail.com>
2021-09-03 21:18:08 +0200

    [fix]: default variable value

--- melrpa/Check_components.py
+++ melrpa/Check_components.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 # ./Check_components.py media/screenshots/components_npy/image0003.png.npy
 
-param_path = sys.argv[1] if len(sys.argv) > 1 else "media/screenshots/components_npy/image0003.png.npy"
+param_path = sys.argv[1] if len(sys.argv) > 1 else "media/screenshots/components_npy/image1.png.npy"
 
 
 recortes = np.load(param_path, allow_pickle=True)
